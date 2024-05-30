b8ff856cedbfc68da4cf5d0d93c0a3a96e99b117
Nicolas Demianiw <ndemia@fastmail.com>
2024-05-30 12:24:49 +0200

    Apply MaterialUI

--- src/components/Forms/Report/ReportForm.tsx
+++ src/components/Forms/Report/ReportForm.tsx
@@ -1,89 +1,67 @@
-import React, { useState } from "react"; // Import React
-import "./reportForm.css";
-import Heading from "../Heading/Heading";
-import BayesianABTest from "../BayesionABtest/BayesianAB";
+import React, { useState } from 'react'; // Import React
+import './reportForm.css';
+import Heading from '../Heading/Heading';
+import BayesianABTest from '../BayesionABtest/BayesianAB';
+import { Button, TextField } from '@mui/material';
 
 interface ReportFormProps {
-  reportTitle: string;
-  setReportTitle: (title: string) => void;
-  isItFinished: (finished: boolean) => void; // Now expects a boolean parameter
+	reportTitle: string;
+	setReportTitle: (title: string) => void;
+	isItFinished: (finished: boolean) => void; // Now expects a boolean parameter
 }
 
-const ReportForm: React.FC<ReportFormProps> = ({
-  reportTitle,
-  setReportTitle,
-  isItFinished,
-}) => {
-  // const [image, setImage] = useState<string | null>(null);
+const ReportForm: React.FC<ReportFormProps> = ({ reportTitle, setReportTitle, isItFinished }) => {
+	// const [image, setImage] = useState<string | null>(null);
 
-  // const handleUploadImage = (event: React.ChangeEvent<HTMLInputElement>) => {
-  //   const file = event.target.files && event.target.files[0];
-  //   const reader = new FileReader();
+	// const handleUploadImage = (event: React.ChangeEvent<HTMLInputElement>) => {
+	//   const file = event.target.files && event.target.files[0];
+	//   const reader = new FileReader();
 
-  //   reader.onload = (upload: ProgressEvent<FileReader>) => {
-  //     if (upload.target && typeof upload.target.result === "string") {
-  //       setImage(upload.target.result);
-  //     }
-  //   };
+	//   reader.onload = (upload: ProgressEvent<FileReader>) => {
+	//     if (upload.target && typeof upload.target.result === "string") {
+	//       setImage(upload.target.result);
+	//     }
+	//   };
 
-  //   if (file) {
-  //     reader.readAsDataURL(file);
-  //   }
-  // };
+	//   if (file) {
+	//     reader.readAsDataURL(file);
+	//   }
+	// };
 
-  const handleButtonClick = () => {
-    const inputTitle = document.getElementById(
-      "report-title"
-    ) as HTMLInputElement;
-    if (inputTitle && inputTitle.value.length > 0) {
-      setReportTitle(inputTitle.value);
+	const handleButtonClick = () => {
+		const inputTitle = document.getElementById('report-title') as HTMLInputElement;
+		if (inputTitle && inputTitle.value.length > 0) {
+			setReportTitle(inputTitle.value);
 
-      isItFinished(true);
-    }
-  };
+			isItFinished(true);
+		}
+	};
 
-  return (
-    <>
-      <Heading title={"Report data"}></Heading>
-      <form className="report-form">
-        <label htmlFor="report-title">
-          Report title
-          <input
-            type="text"
-            name="report-title"
-            id="report-title"
-            defaultValue={reportTitle}
-          />
-        </label>
-        <label htmlFor="report-hypothesis">
-          Hypothesis
-          <textarea name="report-hypothesis" id="report-hypothesis"></textarea>
-        </label>
-        <label htmlFor="report-changes">
-          Changes
-          <textarea name="report-changes" id="report-changes"></textarea>
-        </label>
-        <div className="form-half">
-          <label htmlFor="report-runtime">
-            Runtime
-            <input type="text" name="report-runtime" id="report-runtime" />
-          </label>
-          <label htmlFor="report-targeting">
-            Targeting
-            <input type="text" name="report-targeting" id="report-targeting" />
-          </label>
-        </div>
-        <div className="form-half">
-          <label htmlFor="report-location">
-            Location
-            <input type="text" name="report-location" id="report-location" />
-          </label>
-          <label htmlFor="report-runtime">
-            Runtime again
-            <input type="text" name="report-runtime" id="report-runtime" />
-          </label>
+	return (
+		<>
+			<Heading title={'Report data'}></Heading>
+			<form className="report-form">
+				<TextField
+					label="Report title"
+					variant="outlined"
+					fullWidth
+					type="text"
+					margin="dense"
+					name="report-title"
+					id="report-title"
+					defaultValue={reportTitle}
+				/>
+				<TextField label="Hypothesis" variant="outlined" multiline margin="dense" fullWidth name="report-hypothesis" id="report-hypothesis" />
+				<TextField label="Changes" variant="outlined" multiline margin="dense" fullWidth name="report-changes" id="report-changes" />
 
-          {/* <label htmlFor="report-image">
+				<div className="form-half">
+					<TextField label="Runtime" variant="outlined" margin="dense" name="report-runtime" id="report-runtime" />
+					<TextField label="Targeting" variant="outlined" margin="dense" name="report-targeting" id="report-targeting" />
+					<TextField label="Location" variant="outlined" margin="dense" name="report-location" id="report-location" />
+					<TextField label="Runtime again" variant="outlined" margin="dense" name="report-runtime-again" id="report-runtime-again" />
+				</div>
+
+				{/* <label htmlFor="report-image">
             Upload image
             <input type="file" accept="image/*" onChange={handleUploadImage} />
             {image && (
@@ -94,22 +72,14 @@ const ReportForm: React.FC<ReportFormProps> = ({
               />
             )}
           </label> */}
-        </div>
 
-        <div className=""></div>
-
-        <button
-          type="button"
-          id="report-data-submit"
-          className="report-button"
-          onClick={handleButtonClick}
-        >
-          Next
-        </button>
-      </form>
-      <BayesianABTest />
-    </>
-  );
+				<Button variant="contained" type="button" id="report-data-submit" className="report-button" onClick={handleButtonClick}>
+					Next
+				</Button>
+			</form>
+			<BayesianABTest />
+		</>
+	);
 };
 
 export default ReportForm;
